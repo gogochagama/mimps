354314b42ca396ba81b591ddb6767e1d15b3064f
gogochagama <neko6309@gmail.com>
2018-06-05 04:33:40 -0400

    This is a simple python game

---
+++ moody_mimps.py
@@ -0,0 +1,46 @@
+
+
+
+
+color = str(input("Enter one of six colors  to determine your moody mimp: your color choices are orange, red, blue, green, yellow, gray and pink:")) 
+
+
+
+if color =="orange":
+		print ("Sang is happy")
+     
+
+elif color =="blue":
+
+		print ("Lako  is sad")
+
+
+elif color =="yellow":
+		print ("Hobs is afraid")
+
+
+elif color =="red":
+		print ("Dana is angry")
+
+
+
+elif color =="green":
+		print ("Po is jealous")
+
+
+		
+			
+elif color =="pink":
+		print ("Rochelle is shy")
+		
+
+elif color =="gray":
+		print ("Po is doubtful")			
+
+
+
+
+else:
+		print ("Amos is neutral")
+
+
